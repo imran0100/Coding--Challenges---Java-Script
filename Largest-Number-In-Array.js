a9d5fd5299220ea23984236c1f38a8387b4a2d07
Imran Ansari <i4imran322@gmail.com>
2022-12-03 17:56:19 +0530

    commit

--- Largest-Number-In-Array.js
+++ Largest-Number-In-Array.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let arr = [4, 6, 5, 10, 11, 9, 15, 10];
+let max = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+  if (max < arr[i]) {
+    max = arr[i];
+  }
+}
+console.log(max);
