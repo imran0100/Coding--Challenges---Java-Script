a1b06737721b6e273a73694e00c0e4f1ccf4d468
Imran Ansari <i4imran322@gmail.com>
2022-12-08 20:40:19 +0530

    final commit

--- Move-Zeroes-Leetcode.js
+++ Move-Zeroes-Leetcode.js
@@ -6,11 +6,12 @@
 // Input: nums = [0,1,0,3,12]
 // Output: [1,3,12,0,0]
 
-let nums = [0, 0, 4, 5, 6];
+let nums = [7, 0, 4, 5, 6];
 
-for (let i of nums) {
-  if (nums[i] === 0) {
-    nums.push(Number(nums.splice(i, 1).join()));
-  }
-}
-console.log(nums);
+// for (let i of nums) {
+//   if (nums[i] === 0) {
+//     nums.push(Number(nums.splice(i, 1).join()));
+//   }
+// }
+// console.log(nums);
+console.log(Math.max(a, b));
