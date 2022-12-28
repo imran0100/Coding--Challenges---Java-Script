ea30e185634ade2d5f7ef324ea7283e3bd8eedd4
Imran Ansari <i4imran322@gmail.com>
2022-12-28 17:29:56 +0530

    final

--- map,reduce,filter.js
+++ map,reduce,filter.js
@@ -1,3 +1,33 @@
+const users = [
+  { fname: "Bruce", lname: "Wayne", age: 25 },
+  { fname: "Captain", lname: "America", age: 23 },
+  { fname: "Black", lname: "Panther", age: 27 },
+  { fname: "Clark", lname: "Kent", age: 22 },
+  { fname: "Blk", lname: "Widow", age: 29 },
+  { fname: "Winter", lname: "Soldier", age: 22 },
+  { fname: "Star", lname: "Lord", age: 25 },
+  { fname: "Peter", lname: "Parker", age: 23 },
+  { fname: "Captain", lname: "India", age: 22 },
+  { fname: "Black", lname: "Bitch", age: 27 },
+  { fname: "Thor", lname: "Odinson", age: 22 },
+  { fname: "Captain", lname: "Yellow", age: 33 },
+  { fname: "Winter", lname: "summer", age: 32 },
+  { fname: "Star", lname: "wars", age: 24 },
+];
+
+const result = users.map((user) => user.fname + " " + user.lname);
+console.log("Full name of users:", result);
+
+const result1 = users.filter((user) => user.age > 20 && user.age < 25);
+console.log("Users between 20-26:", result1);
+
+const result2 = users.reduce((acc, curr) => {
+  if (!acc[curr.age]) acc[curr.age] = 1;
+  else acc[curr.age]++;
+  return acc;
+}, {});
+console.log("Age wise count:", result2);
+
 const result3 = users.reduce((user, curr) => {
   if (user[curr.fname]) user[curr.fname]++;
   else user[curr.fname] = 1;
