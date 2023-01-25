e348b0e1c29221b77476d6b5c1e3397b84713f37
Imran Ansari <i4imran322@gmail.com>
2023-01-26 03:28:00 +0530

    Final Commit

--- Maximum-Subarray-Leetcode.js
+++ Maximum-Subarray-Leetcode.js
@@ -1,9 +1,8 @@
 // Given an integer array nums, find the
 // subarray
 //  with the largest sum, and return its sum.
-
+////////
 // Example 1:
-
 // Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 // Output: 6
 // Explanation: The subarray [4,-1,2,1] has the largest sum 6.
@@ -26,5 +25,6 @@ var maxSubArray = function (nums) {
     else sum += nums[i];
     if (sum > max) max = sum;
   }
+
   return max;
 };
