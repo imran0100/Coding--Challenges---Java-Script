26485acfe205ca9555ef979e66328f570d19301b
Imran Ansari <i4imran322@gmail.com>
2023-01-28 20:40:44 +0530

    Final Commit

--- Binary-Search-Leetcode.js
+++ Binary-Search-Leetcode.js
@@ -12,3 +12,15 @@
 // Input: nums = [-1,0,3,5,9,12], target = 2
 // Output: -1
 // Explanation: 2 does not exist in nums so return -1
+
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] == target) return mid;
+    else if (target > nums[mid]) left = mid + 1;
+    else if (target < nums[mid]) right = mid - 1;
+  }
+  return -1;
+};
