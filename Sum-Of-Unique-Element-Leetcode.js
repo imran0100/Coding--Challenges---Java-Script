2717b34860c3acb35b22635a2b3a29c30b0e026f
Imran Ansari <i4imran322@gmail.com>
2023-01-30 18:22:53 +0530

    Final Commit

--- Sum-Of-Unique-Element-Leetcode.js
+++ Sum-Of-Unique-Element-Leetcode.js
@@ -17,3 +17,16 @@
 // Input: nums = [1,2,3,4,5]
 // Output: 15
 // Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+
+var sumOfUnique = function (nums) {
+  let HashMap = {},
+    unique = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (!HashMap[nums[i]]) HashMap[nums[i]] = 1;
+    else HashMap[nums[i]] += 1;
+  }
+  Object.entries(HashMap).forEach(([key, val]) => {
+    if (val == 1) unique.push(Number(key));
+  });
+  return unique.reduce((a, b) => a + b, 0);
+};
