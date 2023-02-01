16a472ff60c561048dc27cd2bb4489402ea06c67
Imran Ansari <i4imran322@gmail.com>
2023-02-02 00:01:45 +0530

    Final Commit

--- 19-Leetcode.js
+++ 19-Leetcode.js
@@ -1,6 +1,6 @@
 // Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
-// Example 1:
+// Example 1:ghghgh
 
 // Input: head = [1,2,3,4,5], n = 2
 // Output: [1,2,3,5]
