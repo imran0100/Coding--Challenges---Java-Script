8bb6018e977f186ec33d1b60bc1698d1cf2de8cd
Imran Ansari <i4imran322@gmail.com>
2023-02-09 23:59:33 +0530

    final commit

---
+++ ppp.js
@@ -0,0 +1,10 @@
+let nums = [15, 6, 5, 7];
+var differenceOfSum = function (nums) {
+  // digits = nums.join(" ");
+  console.log(nums.join(""));
+  for (var i = 0; i < digits.length; i++) {
+    digits[i] = +digits[i];
+  }
+  console.log(digits);
+};
+differenceOfSum();
