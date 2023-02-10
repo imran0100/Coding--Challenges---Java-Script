777bef678970122bba9a1c05d6d564039bce6fff
Imran Ansari <i4imran322@gmail.com>
2023-02-11 00:39:14 +0530

    final commit

---
+++ practice.js
@@ -0,0 +1,20 @@
+// function ab() {
+//   return "hii" + " " + this.name;
+// }
+// let obj = { name: "rahul" };
+// let x = ab.call(obj);
+// console.log(x);
+
+// function ac(message) {
+//   return "hii" + message + this.name;
+// }
+// let obj = { name: "rahul" };
+// let x = ac.call(obj, "hello");
+// console.log(x);
+
+function ac(message) {
+  return "hii" + message + this.name;
+}
+let obj = { name: "rahul" };
+let x = ac.apply(obj, ["helo"]);
+console.log(x);
