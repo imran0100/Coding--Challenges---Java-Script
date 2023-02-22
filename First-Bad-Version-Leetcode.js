19cd0e2c20746426172ae8b81ae6c78b0a85fe2b
Imran Ansari <i4imran322@gmail.com>
2023-02-23 00:59:30 +0530

    Final commit

--- First-Bad-Version-Leetcode.js
+++ First-Bad-Version-Leetcode.js
@@ -17,3 +17,20 @@
 
 // Input: n = 1, bad = 1
 // Output: 1
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 1,
+      right = n;
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (isBadVersion(mid)) right = mid - 1;
+      else left = mid + 1;
+    }
+    if (isBadVersion(left)) return left;
+    return left + 1;
+  };
+};
